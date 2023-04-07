b4a0e45ec2781e511f442b445e931e8b23cc36f8
Charles <charles@notilac.fr>
2023-04-07 16:15:19 +0200

    MAJ LANG

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,47 +1,47 @@
 <?php
 
 return [
-    "job" => [
-        "create_title" => "Create Job",
-        "label" => "Job",
-        "list_title" => "Manage Jobs",
-        "preview_title" => "Preview Job",
-        "update_title" => "Edit Job"
+    'job' => [
+        'create_title' => 'Create Job',
+        'label' => 'Job',
+        'list_title' => 'Manage Jobs',
+        'preview_title' => 'Preview Job',
+        'update_title' => 'Edit Job'
     ],
-    "jobs" => [
-        "complete" => "Terminé",
-        "delete_confirm" => "Voulez vous vraiment supprimer le JOB ?",
-        "delete_selected_confirm" => "Supprimer le Job selectioné ?",
-        "delete_selected_empty" => "Il n' y a pas de JOB à supprimer.",
-        "delete_selected_success" => "Le JOB a bien été supprimé",
-        "error" => "Planté !",
-        "in_progress" => "En cours",
-        "in_queue" => "En attente",
-        "menu_label" => "Jobs",
-        "return_to_list" => "Retour sur les Job",
-        "stopped" => "Arreté",
-        "unknown" => "Erreur inconnu"
+    'jobs' => [
+        'complete' => 'Terminé',
+        'delete_confirm' => 'Voulez vous vraiment supprimer le JOB ?',
+        'delete_selected_confirm' => 'Supprimer le Job selectioné ?',
+        'delete_selected_empty' => 'Il n\' y a pas de JOB à supprimer.',
+        'delete_selected_success' => 'Le JOB a bien été supprimé',
+        'error' => 'Planté !',
+        'in_progress' => 'En cours',
+        'in_queue' => 'En attente',
+        'menu_label' => 'Jobs',
+        'return_to_list' => 'Retour sur les Job',
+        'stopped' => 'Arreté',
+        'unknown' => 'Erreur inconnu'
     ],
-    "labels" => [
-        "knobFormWidget" => "Knob FormWidget"
+    'labels' => [
+        'pluginName' => 'JOB'
     ],
-    "permissions" => [
-        "access_jobs" => "Access Jobs",
-        "access_settings" => "Settings access"
+    'permissions' => [
+        'access_settings' => 'Settings access',
+        'tab' => 'tab job'
     ],
-    "popup" => [
-        "btn" => "ici",
-        "confirm_p" => "Pour le voir cliquez",
-        "confirm_title" => "Lot en cours de création"
+    'popup' => [
+        'btn' => 'ici',
+        'confirm_p' => 'Pour le voir cliquez',
+        'confirm_title' => 'Lot en cours de création'
     ],
-    "settings" => [
-        "messaging" => "Notifications"
+    'settings' => [
+        'messaging' => 'Notifications'
     ],
-    "view" => [
-        "created_at" => "Créer le",
-        "key" => "Clef",
-        "last_update" => "Derniere MAJ",
-        "status" => "Statut",
-        "value" => "Valeur"
+    'view' => [
+        'created_at' => 'Créer le',
+        'key' => 'Clef',
+        'last_update' => 'Derniere MAJ',
+        'status' => 'Statut',
+        'value' => 'Valeur'
     ]
 ];
